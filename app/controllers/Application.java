213097e8b6e1b21e8b0a1bb35ad5c1129f178e31
tpearson <tadhg.pearson@amadeus.com>
2014-03-22 11:53:53 -0400

    API sketch

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -1,14 +1,22 @@
 package controllers;
 
-import play.*;
-import play.mvc.*;
+import com.fasterxml.jackson.databind.node.ObjectNode;
 
-import views.html.*;
+import play.libs.Json;
+import play.mvc.Controller;
+import play.mvc.Result;
+import views.html.index;
 
 public class Application extends Controller {
 
     public static Result index() {
         return ok(index.render("Your new application is ready."));
+    }
+    
+    public static Result flightSearch(String origin, String destination, String departureDate, String arrivalDate){
+    	ObjectNode out = Json.newObject();
+    	out.put("output", Json.newObject());
+		return ok(out);
     }
 
 }
