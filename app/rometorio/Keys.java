d020efd5c6e40961351103fd5be600c8611a3446
tpearson <tadhg.pearson@amadeus.com>
2014-03-22 16:40:34 -0400

    add keys

---
+++ app/rometorio/Keys.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package rometorio;
+
+/**
+ * @author tpearson
+ *
+ */
+public class Keys {
+
+	static final public String skyscannerKey(){
+		return "h4A3uJNEbLyCedh3be6Gai1ngooQyAoM";
+	}
+
+	public static String searchURL() {
+		return "http://api.sandbox.amadeus.com/v1/flight-low-fare-search/masterPricer";
+	}
+}
